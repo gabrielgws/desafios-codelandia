b521d7a79359158b08bbe88934424527b98f8a91
Gabriel Willian <gabrielgws@Suporte.localdomain>
2022-07-14 17:17:49 -0300

    search handleChange

--- desafio-1/src/components/Header/index.jsx
+++ desafio-1/src/components/Header/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
 
 import { AiOutlineSearch } from 'react-icons/ai';
 
 function Header() {
+  const [search, setSearch] = useState("");
+  
+  function handleChange(event){
+    setSearch(event.target.value)
+    console.log(event.target.value)
+  }
+
   return(
     <div className={styles.container}>
       <div className={styles.center}>
@@ -13,7 +20,12 @@ function Header() {
         </div>
         <div className={styles.search}>
           <h2><AiOutlineSearch /></h2>
-          <input type="text" placeholder='Pesquisar no blog' />
+          <input
+            value={search}
+            type="text"
+            placeholder='Pesquisar no blog'
+            onChange={handleChange}
+          />
         </div>
       </div>
     </div>

10882a14190d43d5797b131cb10d3bf6fb37dbd5
Gabriel Willian <gabrielgws@Suporte.localdomain>
2022-07-14 16:30:10 -0300

    json-server e map add

--- desafio-1/src/components/PostContainer/Index.jsx
+++ desafio-1/src/components/PostContainer/Index.jsx
@@ -1,59 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
+
+// yarn json-server --watch data/db.json --port 8000
 
 import { AiOutlineHeart } from 'react-icons/ai';
 
 function PostContainer() {
-  return (
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blog/')
+    .then(response => response.json())
+    .then(data => setApi(data))
+  }, []);
+
+   return (
     <div className={styles.container}>
-      <div className={styles.center}>
-          <div className={styles.dateHeart}>
-            <h2>12 de jul, 2021</h2>
-            <p><AiOutlineHeart/></p>
-          </div>
+        {api.map((setApi) => (
+          <>
+            <div className={styles.center}>
+              <div className={styles.dateHeart}>
+                <h2>{setApi.data}</h2>
+                <p><AiOutlineHeart/></p>
+              </div>
 
-          <div className={styles.posts}>
-            <h1>Agora é oficial: o Windows 11 está vindo</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis.</p>
-          </div>
+              <div className={styles.posts}>
+                <h1>{setApi.title}</h1>
+                <p>{setApi.body}</p>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
-
-      <div className={styles.center}>
-          <div className={styles.dateHeart}>
-            <h2>11 de jul, 2021</h2>
-            <p><AiOutlineHeart/></p>
-          </div>
-
-          <div className={styles.posts}>
-            <h1>Tim Berners-Lee vai leiloar código-fonte da web</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.</p>
-          </div>
-      </div>
-
-      <div className={styles.center}>
-          <div className={styles.dateHeart}>
-            <h2>10 de jul, 2021</h2>
-            <p><AiOutlineHeart/></p>
-          </div>
-
-          <div className={styles.posts}>
-            <h1>Tem Firefox novo no pedaço e você vai querer migrar</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est.</p>
-          </div>
-      </div>
-
-      <div className={styles.center}>
-          <div className={styles.dateHeart}>
-            <h2>09 de jul, 2021</h2>
-            <p><AiOutlineHeart/></p>
-          </div>
-
-          <div className={styles.posts}>
-            <h1>John McAfee, criador do antivírus McAfee, morre</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vestibulum auctor est. Nam vitae finibus ante. Duis lobortis tellus vel diam fringilla, eu ullamcorper ex iaculis. Praesent et auctor justo. Vestibulum nisl orci, lacinia venenatis leo sit amet, pulvinar tincidunt risus. Phasellus nisl dui, cursus a lectus et, interdum ullamcorper libero.</p>
-          </div>
-      </div>
-    </div>
   );
 }
 
